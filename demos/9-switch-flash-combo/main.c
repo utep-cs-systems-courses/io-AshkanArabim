bfe5d457c96ba208a3513610e0b923ac2ddc220c
AshkanArabim <ashkan.arabim@gmail.com>
2023-11-06 13:47:46 -0700

    demo 9 state machine

--- demos/9-switch-flash-combo/main.c
+++ demos/9-switch-flash-combo/main.c
@@ -8,11 +8,15 @@
 #define SW1 BIT3		/* switch1 is p1.3 */
 #define SWITCHES SW1		/* only 1 switch on this board */
 
+// global state variable
+int state = 0;
+
 void switch_init() {
   P1REN |= SWITCHES;		/* enables resistors for switches */
   P1IE |= SWITCHES;		/* enable interrupts from switches */
   P1OUT |= SWITCHES;		/* pull-ups for switches */
   P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+  P1IES |= SWITCHES;
 }
 
 void led_init() {
@@ -41,19 +45,15 @@ switch_interrupt_handler()
 {
   char p1val = P1IN;		/* switch is in P1 */
 
-/* update switch interrupt sense to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-
-  if (p1val & SW1) {		/* button up */
-    P1OUT &= ~LED_GREEN;
-    buttonDown = 0;
-  } else {			/* button down */
+  state++;
+  if (state >= 5) {
+    // DEBUG
     P1OUT |= LED_GREEN;
-    buttonDown = 1;
+    P1OUT &= ~LED_RED;
+
+    state = 0;
   }
 }
-
 
 /* Switch on P1 (S2) */
 void
@@ -67,14 +67,58 @@ __interrupt_vec(PORT1_VECTOR) Port_1(){
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
-  static int blink_count = 0;
-  switch (blink_count) { 
-  case 6: 
-    blink_count = 0;
-    P1OUT |= LED_RED;
+  static int blink_count = 0; // value retained because it's static
+  blink_count++;
+
+  switch (state)
+  {
+  case 0: // both on
+    P1OUT |= LEDS;
     break;
+
+  case 1: // green on 
+    P1OUT |= LED_GREEN;
+    P1OUT &= ~LED_RED;
+    break;
+
+  case 2: // red on
+    P1OUT |= LED_RED;
+    P1OUT &= ~LED_GREEN;
+    break;
+
+  case 3: // alternating red and green
+    if (blink_count < 125) {
+      // for half a second red is on
+      P1OUT |= LED_RED;
+      P1OUT &= ~LED_GREEN;
+    } else {
+      // for half a second green is on
+      P1OUT |= LED_GREEN;
+      P1OUT &= ~LED_RED;
+    }
+    break;
+
+  case 4: // all off
+    P1OUT &= ~LEDS;
+    break;
+  
   default:
-    blink_count ++;
-    if (!buttonDown) P1OUT &= ~LED_RED; /* don't blink off if button is down */
+    P1OUT &= ~LEDS;
+    state = 0;
+    break;
   }
+
+  if (blink_count > 250) { // I don't care about more than 1 sec.
+    blink_count = 0;
+  }
+
+  // switch (blink_count) { 
+  // case 6: 
+  //   blink_count = 0;
+  //   P1OUT |= LED_RED;
+  //   break;
+  // default:
+  //   blink_count ++;
+  //   if (!buttonDown) P1OUT &= ~LED_RED; /* don't blink off if button is down */
+  // }
 } 
